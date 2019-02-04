19e2adccae6219f7aa8f0b5a432cc6801d1292ef
Ivan Pugach <ivan.pugach@spryker.com>
2019-02-04 23:45:02 +0200

    Update code after first round of AR

--- src/SprykerShop/Yves/QuickOrderPageExtension/Dependency/Plugin/QuickOrderFileParserStrategyPluginInterface.php
+++ src/SprykerShop/Yves/QuickOrderPageExtension/Dependency/Plugin/QuickOrderFileParserStrategyPluginInterface.php
@@ -9,7 +9,7 @@ namespace SprykerShop\Yves\QuickOrderPageExtension\Dependency\Plugin;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-interface QuickOrderFileProcessorStrategyPluginInterface
+interface QuickOrderFileParserStrategyPluginInterface
 {
     /**
      * Specification:
@@ -22,41 +22,6 @@ interface QuickOrderFileProcessorStrategyPluginInterface
      * @return bool
      */
     public function isApplicable(UploadedFile $file): bool;
-
-    /**
-     * Specification:
-     * - Return allowed mime types of plugin.
-     *
-     * @api
-     *
-     * @return string[]
-     */
-    public function getAllowedMimeTypes();
-
-    /**
-     * Specification:
-     * - Check if file format valid.
-     *
-     * @api
-     *
-     * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     *
-     * @return bool
-     */
-    public function isValidFormat(UploadedFile $file): bool;
-
-    /**
-     * Specification:
-     * - Check if amount of rows are valid.
-     *
-     * @api
-     *
-     * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     * @param int $maxAllowedLines
-     *
-     * @return bool
-     */
-    public function isValidAmountOfRows(UploadedFile $file, int $maxAllowedLines): bool;
 
     /**
      * Specification:
